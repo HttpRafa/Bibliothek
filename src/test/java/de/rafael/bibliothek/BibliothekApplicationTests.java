f411b6f75aba058c59d3de0814212c9f342ac911
HttpRafa <rafael@kienitz.link>
2023-06-12 00:43:22 +0200

    Added empty spring project

---
+++ src/test/java/de/rafael/bibliothek/BibliothekApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rafael.bibliothek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
